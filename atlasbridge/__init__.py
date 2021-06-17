e40d72f6779ed25dccf14eea02842013942e0d51
Dylan Armitage <dylan.armitage@fresnounified.org>
2021-06-17 10:56:20 -0700

    Add package information

---
+++ atlasbridge/__init__.py
@@ -0,0 +1,19 @@
+from typing import Dict, Any
+
+_package_data = dict(
+    full_package_name="atlasbridge",
+    version_info=(0, 0, 0),
+    __version__="0.0.0",
+    author="Dylan Armitage",
+    author_email="dylanjarmitage@gmail.com",
+    description="AtlasBridge processes grades exported from different services, allowing them to be uploaded to ATLAS",  # NOQA
+    since=2021,
+    keywords="yaml 1.2 parser round-trip preserve quotes order config",
+    read_the_docs="yaml",
+    supported=[(3, 6)],  # minimum tested
+    python_requires=">=3",
+)  # type: Dict[Any, Any]
+
+
+version_info = _package_data["version_info"]
+__version__ = _package_data["__version__"]
