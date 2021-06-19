712044078ee252b5852ec258454a9e16db757b5d
Dylan Armitage <dylanjarmitage@gmail.com>
2021-06-18 21:24:33 -0700

    Add config file validity test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,8 @@
 import sys, os
 
-from hypothesis import given, strategies as st
+import pytest
+from hypothesis import given, assume, strategies as st
+from pathlib import Path
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + "/../")
@@ -30,5 +32,19 @@ def test_config_import_export(c):
     assert in_dict == c
 
 
-def test_config_validity():
-    assert True
+def test_config_file_validity(dne):
+    config_dir = Path("data/config")
+    config_files = config_dir.glob("*.yml")
+    only_required = ["db.yml"]  # Only test required fields
+    req_fields = ["expected_extensions", "rows", "columns"]
+    other_fields = ["contains_multiple", "needs_matching", "match_with"]
+    column_fields = ["name", "name_pattern", "ID", "username"]
+    for file in config_files:
+        file_data = config.import_config(path=file)
+        for field in req_fields:
+            assert field in file_data
+        for column in column_fields:
+            assert column in file_data["columns"]
+        if file.name not in only_required:
+            for field in other_fields:
+                assert field in file_data
