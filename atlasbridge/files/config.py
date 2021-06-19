afaa64ab2c6ea35551006bfc5e326c72a2058f1c
Dylan Armitage <dylanjarmitage@gmail.com>
2021-06-18 22:01:20 -0700

    Add logging to config.py

--- atlasbridge/files/config.py
+++ atlasbridge/files/config.py
@@ -1,18 +1,26 @@
+import logging
 from ruamel.yaml import YAML
 from pathlib import Path
 from typing import Dict, Any, Union, TextIO
 
+logger = logging.getLogger(__name__)
+
 
 def import_config(path: Union[Path, TextIO]) -> Dict[str, Any]:
+    logging.debug("Reading yaml config")
     yaml = YAML(typ="safe")
     yaml.allow_unicode = True
     config = yaml.load(path)
+    logging.debug(f"Read config: {config}")
     if config is None:
         config = {}
+    logger.info(f"Config: {config}")
     return config
 
 
 def write_config(conf: Dict[str, Any], path: Union[Path, TextIO]) -> None:
+    logger.debug(f"Writing config: {conf}")
     yaml = YAML()
     yaml.allow_unicode = True
     yaml.dump(conf, path)
+    logger.info(f"Wrote config to {path}")
