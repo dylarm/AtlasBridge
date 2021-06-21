e45ec1c74e3100d66f1ebeef97326e6b336f2ed1
Dylan Armitage <dylan.armitage@fresnounified.org>
2021-06-21 11:07:51 -0700

    Add extension checking
    The utility of which will hopefully be useful -> Ideally, the extension will tell us which reader to use. And if the extension is expected, then it's more likely to succeed (aka, not some weird malformed file where someone just renamed it instead).

--- atlasbridge/files/data.py
+++ atlasbridge/files/data.py
@@ -6,6 +6,11 @@ from typing import Dict, Any
 logger = logging.getLogger(__name__)
 
 
+def __check_extension(path: Path, conf: Dict[str, Any]) -> bool:
+    logger.debug(f"Testing extension for {path.name}")
+    return path.suffix in conf["expected_extensions"]
+
+
 def __read_excel(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
     pass
 
@@ -15,6 +20,10 @@ def __read_csv(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
 
 
 def __read_zip(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
+    pass
+
+
+def __read_file(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
     pass
 
 
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,13 +1,32 @@
 import sys, os
 
 import pytest
-from hypothesis import given, assume, strategies as st
+from hypothesis import given, note, strategies as st
 from pathlib import Path
+from typing import List
+from test_config import CHAR_CAT
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + "/../")
 
 from atlasbridge.files import data
+
+
+@given(
+    ext=st.characters(whitelist_categories=CHAR_CAT),
+    config=st.lists(st.characters(whitelist_categories=CHAR_CAT)),
+)
+def test_extension_check(ext: str, config: List[str]):
+    t_dict = {"expected_extensions": config}
+    t_file = Path("test_file." + ext)
+    if ext not in config:
+        ext_in_dict = data.__check_extension(path=t_file, conf=t_dict)
+        note(f"Extension NOT in dict result: {ext_in_dict}")
+        assert ext_in_dict is False
+    else:
+        ext_in_dict = data.__check_extension(path=t_file, conf=t_dict)
+        note(f"Extension IS in dict result: {ext_in_dict}")
+        assert ext_in_dict is True
 
 
 def test_read_excel():
