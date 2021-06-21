f3a49819df8ee764856d3d0de74e7c9527c4f92a
Dylan Armitage <dylan.armitage@fresnounified.org>
2021-06-21 10:14:28 -0700

    Add __read_*() functions to files/data.py
    Abstract out the actual reading of files.

--- atlasbridge/files/data.py
+++ atlasbridge/files/data.py
@@ -6,9 +6,21 @@ from typing import Dict, Any
 logger = logging.getLogger(__name__)
 
 
-def load_database(path: Path, conf: Dict[str, Any]):
+def __read_excel(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
     pass
 
 
-def load_grade_file(path: Path, conf: Dict[str, Any]):
+def __read_csv(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
+    pass
+
+
+def __read_zip(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
+    pass
+
+
+def load_database(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
+    pass
+
+
+def load_grade_file(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
     pass
--- tests/test_data.py
+++ tests/test_data.py
@@ -10,6 +10,18 @@ sys.path.insert(0, myPath + "/../")
 from atlasbridge.files import data
 
 
+def test_read_excel():
+    assert True
+
+
+def test_read_csv():
+    assert True
+
+
+def test_read_zip():
+    assert True
+
+
 # Will need to create mock in-memory files for testing purposes
 # https://coderbook.com/@marcus/how-to-mock-and-unit-test-with-pandas/
 def test_load_database():
