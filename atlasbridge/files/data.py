4c58aba8233ac0ae77cbbbb41ac5f7cf5c3b8b91
Dylan Armitage <dylan.armitage@fresnounified.org>
2021-06-21 14:04:25 -0700

    Add __read_file() for all valid extensions

--- atlasbridge/files/data.py
+++ atlasbridge/files/data.py
@@ -30,7 +30,25 @@ def __read_zip(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
 
 
 def __read_file(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
-    pass
+    ext_expected = __check_extension(path, conf)
+    extension = path.suffix
+    if ext_expected:
+        logger.info(f"Extension {extension} was expected for {path.name}")
+    else:
+        logger.warning(
+            f"Extension {extension} was NOT expected for {path.name}"
+            f"Expected one of: {conf['expected_extensions']}"
+        )
+    if extension is READING_EXTENSIONS[0]: # csv
+        db = __read_csv(path, conf)
+    elif extension is READING_EXTENSIONS[1] or extension is READING_EXTENSIONS[2]: # Excel
+        db = __read_excel(path, conf)
+    elif extension is READING_EXTENSIONS[3]: # zip
+        db = __read_zip(path, conf)
+    else:
+        logger.error(f"No reader found for {extension} !")
+        db = pd.DataFrame()
+    return db
 
 
 def load_database(path: Path, conf: Dict[str, Any]) -> pd.DataFrame:
